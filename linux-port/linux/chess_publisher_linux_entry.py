9924970e712de528d27c582cc29326fcad2fc007
kbilyal <65768770+kbilyal@users.noreply.github.com>
2026-09-07 23:24:16 +0300

    Linux dev6: activate FIDE payload policy

--- linux-port/linux/chess_publisher_linux_entry.py
+++ linux-port/linux/chess_publisher_linux_entry.py
@@ -6,6 +6,7 @@ import chess_publisher_linux as app
 from build_info import APP_BUILD,ENGINE_VERSION
 from build_identity_integration import apply as apply_build_identity
 from source_guard import require_package_source,SourceIdentityError
+from fide_payload_policy import apply as apply_fide_payload_policy
 from fide_integration import apply as apply_fide
 from chess_results_integration import apply as apply_chess_results
 from dgt_integration import apply as apply_dgt
@@ -13,6 +14,7 @@ from telegram_integration import apply as apply_telegram
 from desktop_integration import apply as apply_desktop
 
 apply_build_identity()
+apply_fide_payload_policy()
 apply_fide()
 apply_chess_results()
 apply_dgt()
