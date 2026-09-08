50720e894020ff08cc6ebcfe907d5b0455f299ce
kbilyal <65768770+kbilyal@users.noreply.github.com>
2026-09-08 18:44:14 +0300

    Bump Linux fluid UI build to dev20

--- linux-port/linux/build_info.py
+++ linux-port/linux/build_info.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Single source of truth for the Chess-Publisher Linux development build identity."""
-APP_BUILD = "1.06.00-beta.34-linux-dev.19"
+APP_BUILD = "1.06.00-beta.34-linux-dev.20"
 ENGINE_VERSION = "0.6.1-linux-dev"
-DEB_VERSION = "1.06.00~beta34+linuxdev19"
-DISPLAY_VERSION = "v1.06.00-beta.34-linuxdev19"
+DEB_VERSION = "1.06.00~beta34+linuxdev20"
+DISPLAY_VERSION = "v1.06.00-beta.34-linuxdev20"
